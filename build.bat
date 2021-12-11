760f88fb5a3a9fd5ba3394e1d8a28c4a652d57fa
Yura Vodkin <87276332+Serkuz90@users.noreply.github.com>
2021-12-11 10:38:51 +0400

    add some string
    mvn package

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@ set M2_HOME=E:\apache-maven-3.8.4
 set path=C:\apache-maven-3.8.4\bin;%path%;
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_241
 set path=C:\Program Files\Java\jdk1.8.0_202\bin;%path%;
+mvn package
